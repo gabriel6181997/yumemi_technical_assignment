12dadb77bf05d576f8eb4180d5c5ce295250af60
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-26 23:53:59 +0900

    ✨ Add graph component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
-import type { NextPage } from 'next'
-import { PrefectureChoice } from 'src/components/Layout/PrefectureChoice'
-import { Title } from 'src/components/Layout/Title'
+import type { NextPage } from "next";
+import { Graph } from "src/components/Layout/Graph";
+import { PrefectureChoice } from "src/components/Layout/PrefectureChoice";
+import { Title } from "src/components/Layout/Title";
 
-import styles from '../styles/Home.module.scss'
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Title />
       <PrefectureChoice />
-
+      <div className={styles.graphContainer}>
+        <Graph />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
-export default Home
+export default Home;
