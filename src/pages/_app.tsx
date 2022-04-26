b560f247a75e8d57e84bb4df2d1cfcfb54bc4bc9
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-26 16:08:30 +0900

    🎨 Change code structure based on the rules of ESLint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import type { AppProps } from "next/app";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default MyApp;
