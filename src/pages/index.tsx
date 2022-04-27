b6a723ae393cb7abe830b41b8ea9356b999eb0c6
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-27 23:10:34 +0900

    👽️ Fetch data from prefectureAPI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,36 @@
 import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
 import { Graph } from "src/components/Layout/Graph";
 import { PrefectureChoice } from "src/components/Layout/PrefectureChoice";
 import { Title } from "src/components/Layout/Title";
+import type { PrefectureData } from "src/lib/type/type";
 
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
+  const [prefectureData, setPrefectureData] = useState<PrefectureData[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [prefectureCheckedList, setPrefectureCheckedList] = useState<number[]>(
+    []
+  );
+
+  const getPrefectureData = useCallback(async () => {
+    const prefectureDataFromAPI = await fetch("/api/prefectureAPI");
+    const prefectureDataJson = await prefectureDataFromAPI.json();
+    setPrefectureData(prefectureDataJson.result);
+  }, []);
+
+  useEffect(() => {
+    getPrefectureData();
+  }, [getPrefectureData]);
+
   return (
     <div className={styles.container}>
       <Title />
-      <PrefectureChoice />
+      <PrefectureChoice
+        prefectureData={prefectureData}
+        setPrefectureCheckedList={setPrefectureCheckedList}
+      />
       <div className={styles.graphContainer}>
         <Graph />
       </div>
