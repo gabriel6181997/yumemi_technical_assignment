9dfe242ca257568e7ba1ac2882e0c933fec79f63
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-28 20:06:49 +0900

    ♻️ Refactor code with the use of Custom Hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,22 @@ import { useCallback, useEffect, useState } from "react";
 import { Graph } from "src/components/Layout/Graph";
 import { PrefectureChoice } from "src/components/Layout/PrefectureChoice";
 import { Title } from "src/components/Layout/Title";
-import type { GraphData, PrefectureData } from "src/lib/type/type";
-import { graphColor } from "src/lib/utils/utils";
+import { useFetchPopulationData } from "src/lib/hooks/useFetchPopulationData";
+import { useGraphLabels } from "src/lib/hooks/useGraphLabels";
+import { usePrefectureData } from "src/lib/hooks/usePrefectureData";
+import type { GraphData } from "src/lib/type/type";
 
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
-  const [prefectureData, setPrefectureData] = useState<PrefectureData[]>([]);
+  const { prefectureData, prefectureDataError } = usePrefectureData();
+  const { getGraphLabels, graphLabels, graphLabelsError } = useGraphLabels();
+  const { fetchPopulationData } = useFetchPopulationData();
   const [prefectureCheckedList, setPrefectureCheckedList] = useState<number[]>(
     []
   );
   const [graphDataList, setGraphDataList] = useState<GraphData[]>([]);
-  const [graphLabels, setGraphLabels] = useState<number[]>([]);
 
-  const getPrefectureData = useCallback(async () => {
-    const prefectureDataFromAPI = await fetch("/api/prefectureAPI");
-    const prefectureDataJson = await prefectureDataFromAPI.json();
-    setPrefectureData(prefectureDataJson.result);
-  }, []);
-
-  // Add Loading
   const getPopulationData = useCallback(async () => {
     const list = prefectureCheckedList.map((prefecture) => {
       return fetchPopulationData(prefecture);
@@ -32,58 +28,22 @@ const Home: NextPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [prefectureCheckedList]);
 
-  const fetchPopulationData = async (prefecture: number) => {
-    const populationDataFromAPI = await fetch(
-      `/api/populationAPI/${prefecture}`
-    );
-    const populationDataJson = await populationDataFromAPI.json();
-    const prefectureName =
-      prefectureData.find((pre) => {
-        return pre.prefCode === prefecture;
-      })?.prefName || "";
-
-    const populationValue = populationDataJson.result.data
-      .find((pop) => {
-        return pop.label === "総人口";
-      })
-      .data.map((d) => {
-        return d.value;
-      });
-
-    const datasetItem = {
-      label: prefectureName,
-      data: populationValue,
-      fill: false,
-      borderColor: graphColor[prefecture],
-    };
-
-    return datasetItem;
-  };
-
-  const getGraphLabels = useCallback(async () => {
-    const populationDataFromAPI = await fetch(`/api/populationAPI/1`);
-    const populationDataJson = await populationDataFromAPI.json();
-    const populationYear = populationDataJson.result.data
-      .find((pop) => {
-        return pop.label === "総人口";
-      })
-      .data.map((d) => {
-        return d.year;
-      });
-    setGraphLabels(populationYear);
-  }, []);
-
-  useEffect(() => {
-    getPrefectureData();
-  }, [getPrefectureData]);
-
   useEffect(() => {
     getPopulationData();
   }, [getPopulationData, prefectureCheckedList]);
 
   useEffect(() => {
     getGraphLabels();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (prefectureDataError instanceof Error) {
+    return <div>{prefectureDataError.message}</div>;
+  }
+
+  if (graphLabelsError instanceof Error) {
+    return <div>{graphLabelsError.message}</div>;
+  }
 
   return (
     <div className={styles.container}>
