e9f95aa2b0084a419cb7ed7bc7696e1b0008b534
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-28 13:14:07 +0900

    🏷️ Add GraphData

--- src/lib/type/type.ts
+++ src/lib/type/type.ts
@@ -1,4 +1,11 @@
 export type PrefectureData = {
-  prefCode: number
-  prefName: string
-}
+  prefCode: number;
+  prefName: string;
+};
+
+export type GraphData = {
+  label: string;
+  data: number;
+  fill: boolean;
+  borderColor: string;
+};
