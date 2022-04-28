3ae157e58339f81316a01093f7840155241d01f2
gabriel6181997 <gabriel6181997@gmail.com>
2022-04-28 14:41:10 +0900

    🚧 Construct graph with the use of data from Population API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,18 @@ import { useCallback, useEffect, useState } from "react";
 import { Graph } from "src/components/Layout/Graph";
 import { PrefectureChoice } from "src/components/Layout/PrefectureChoice";
 import { Title } from "src/components/Layout/Title";
-import type { PrefectureData } from "src/lib/type/type";
+import type { GraphData, PrefectureData } from "src/lib/type/type";
+import { graphColor } from "src/lib/utils/utils";
 
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   const [prefectureData, setPrefectureData] = useState<PrefectureData[]>([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [prefectureCheckedList, setPrefectureCheckedList] = useState<number[]>(
     []
   );
+  const [graphDataList, setGraphDataList] = useState<GraphData[]>([]);
+  const [graphLabels, setGraphLabels] = useState<number[]>([]);
 
   const getPrefectureData = useCallback(async () => {
     const prefectureDataFromAPI = await fetch("/api/prefectureAPI");
@@ -20,9 +22,68 @@ const Home: NextPage = () => {
     setPrefectureData(prefectureDataJson.result);
   }, []);
 
+  // Add Loading
+  const getPopulationData = useCallback(async () => {
+    const list = prefectureCheckedList.map((prefecture) => {
+      return fetchPopulationData(prefecture);
+    });
+    setGraphDataList(await Promise.all(list));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [prefectureCheckedList]);
+
+  const fetchPopulationData = async (prefecture: number) => {
+    const populationDataFromAPI = await fetch(
+      `/api/populationAPI/${prefecture}`
+    );
+    const populationDataJson = await populationDataFromAPI.json();
+    const prefectureName =
+      prefectureData.find((pre) => {
+        return pre.prefCode === prefecture;
+      })?.prefName || "";
+
+    const populationValue = populationDataJson.result.data
+      .find((pop) => {
+        return pop.label === "総人口";
+      })
+      .data.map((d) => {
+        return d.value;
+      });
+
+    const datasetItem = {
+      label: prefectureName,
+      data: populationValue,
+      fill: false,
+      borderColor: graphColor[prefecture],
+    };
+
+    return datasetItem;
+  };
+
+  const getGraphLabels = useCallback(async () => {
+    const populationDataFromAPI = await fetch(`/api/populationAPI/1`);
+    const populationDataJson = await populationDataFromAPI.json();
+    const populationYear = populationDataJson.result.data
+      .find((pop) => {
+        return pop.label === "総人口";
+      })
+      .data.map((d) => {
+        return d.year;
+      });
+    setGraphLabels(populationYear);
+  }, []);
+
   useEffect(() => {
     getPrefectureData();
   }, [getPrefectureData]);
+
+  useEffect(() => {
+    getPopulationData();
+  }, [getPopulationData, prefectureCheckedList]);
+
+  useEffect(() => {
+    getGraphLabels();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -32,7 +93,7 @@ const Home: NextPage = () => {
         setPrefectureCheckedList={setPrefectureCheckedList}
       />
       <div className={styles.graphContainer}>
-        <Graph />
+        <Graph datasets={graphDataList} labels={graphLabels} />
       </div>
     </div>
   );
